d78d5976ca3fee248d1217ee9a7acd76cab78037
Andrian <saw3k19@gmail.com>
2023-09-12 18:34:03 +0300

    homework nr. 13 with java script

---
+++ script.js
@@ -0,0 +1,10 @@
+let countryName = 'Germany';
+let continentName = 'Europe';
+let populationNumber = 84;
+let island = false;
+let countryDescription = `${countryName} is located in ${continentName},${populationNumber} people live here`;
+console.log('countryName:',countryName);
+console.log('continentName:',continentName);
+console.log('populationNumber:',populationNumber);
+console.log(island)
+console.log('countryDescription:',countryDescription);
