8832d488ee8a5e3a062685544bebe22a49deb446
Andrian <saw3k19@gmail.com>
2023-09-24 16:29:01 +0300

    homework 15 exer. Nr 8

--- script.js
+++ script.js
@@ -143,13 +143,19 @@ console.log(assignedGrade(90));
 // homework 15 exercise Nr 7
 
 function tellFortune (N,Z,Y,X) {
-    
      console.log(`You will be ${X} in ${Y}, and will get married to ${Z} with ${N} kids`)
 }
-
 tellFortune(1, "Sophia", "London", "a renowned programmer");
     tellFortune(2, "Emma", "Paris", "a successful entrepreneur");
     tellFortune(3, "Corina", "Rome", "a successful pharmacist");
 
 
+// homework 15 exercise Nr 8
+    function calculatedDogAge(N){ 
+        let dogYears = N * 7; 
+        console.log(`Your dog is ${dogYears} years old in dog years`)
+    }
 
+   calculatedDogAge(5);
+   calculatedDogAge(8);
+   calculatedDogAge(10);
